de7e4c43b846f68581e28d0498d1b7da926a5bfa
it24102928 <it24102928@my.sliit.lk>
2024-08-28 10:05:41 +0530

    IT24102928
    Lab6Q2A

---
+++ Q2/IT24102928Lab6Q2A.java
@@ -0,0 +1,13 @@
+public class IT24102928Lab6Q2A  {
+
+    public static void main(String[] args) {
+
+       int number=1;
+
+       while(number <= 10)  {
+
+       System.out.print(number + " ");
+       number ++;
+         } 
+   }
+ }
